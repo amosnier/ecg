d4641f34398a768d94d1d40cc62c5cad1dc1419e
Alain Mosnier <alain@wanamoon.net>
2020-05-03 22:34:55 +0200

    Added exception catching in unit tests

--- test.py
+++ test.py
@@ -1,14 +1,17 @@
 import os
 import ecg
-
+import sys
 
 def main():
     for subdir, dirs, files in os.walk('svd'):
         for file in files:
             if file.endswith('.svd'):
                 full_path = os.path.join(subdir, file)
-                print(full_path)
-                ecg.generate_code(full_path, None, 'mcu_support')
+                try:
+                    ecg.generate_code(full_path, None, 'mcu_support')
+                except Exception:
+                    sys.stderr.write('--- Caught exception while parsing {} ---\n'.format(full_path))
+                    raise
 
 
 if __name__ == '__main__':
