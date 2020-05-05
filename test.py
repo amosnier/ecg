d513712d02158c58307925a794531376ab22be76
Alain Mosnier <alain@wanamoon.net>
2020-05-05 22:15:11 +0200

    Made test code generate actual files

--- test.py
+++ test.py
@@ -5,13 +5,15 @@ import sys
 
 def main():
     for subdir, dirs, files in os.walk('svd'):
-        for file in files:
-            if file.endswith('.svd'):
-                full_path = os.path.join(subdir, file)
+        for in_file in files:
+            if in_file.endswith('.svd'):
+                out_dir = os.path.join('generated', os.path.relpath(subdir, 'svd'), os.path.splitext(in_file)[0])
+                os.makedirs(out_dir, exist_ok=True)
+                full_in_path = os.path.join(subdir, in_file)
                 try:
-                    ecg.generate_code(full_path, None, 'mcu_support')
+                    ecg.generate_code(full_in_path, os.path.join(out_dir, 'mcu.h'), 'mcu_support')
                 except Exception:
-                    sys.stderr.write('--- Caught exception while parsing {} ---\n'.format(full_path))
+                    sys.stderr.write('--- Caught exception while parsing {} ---\n'.format(full_in_path))
                     raise
 
 
