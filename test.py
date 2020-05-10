92bd3395b85883ba0e06454bfa2b96c8da3f87dc
Alain Mosnier <alain@wanamoon.net>
2020-05-10 20:57:19 +0200

    Implemented compilation test
    Which actually fails on at least one MCU, interesting...
    
    This won't impact Travis however, because the compilation test is not
    yet activated in the Travis configuration.

--- test.py
+++ test.py
@@ -1,9 +1,15 @@
 import os
 import ecg
 import sys
+import argparse
 
 
 def main():
+    parser = argparse.ArgumentParser(description='Test ecg.')
+    parser.add_argument('-c', '--compiler', help='C++ header file name')
+    args = parser.parse_args()
+    compiler = args.compiler
+    os.makedirs('build', exist_ok=True)
     num_files = 0
     for subdir, dirs, files in os.walk('svd'):
         for in_file in files:
@@ -14,6 +20,13 @@ def main():
                 try:
                     full_out_path = os.path.join(out_dir, 'mcu.h')
                     ecg.generate_code(full_in_path, full_out_path, 'mcu_support')
+                    if compiler:
+                        compile_string = '{} -std=c++17 -o {} -c test.cpp -I {}'
+                        object_file = os.path.join('build', 'test.o')
+                        command = compile_string.format(compiler, object_file, out_dir)
+                        print('Running: {}'.format(command))
+                        assert not os.system(command)
+                        print('Compilation successful\n')
                     num_files += 1
                 except Exception:
                     sys.stderr.write('--- Caught exception while parsing {} ---\n'.format(full_in_path))
