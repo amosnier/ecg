f9e3700bc030e4f2a5918aafd8399675f7cf0739
Alain Mosnier <alain@wanamoon.net>
2020-05-10 17:41:15 +0200

    Created cpp-file for compilation test, and removed C++20 requirement
    Bit-field initialization is only available from C++20,
    unfortunately. While it works perfectly with -std=gnu++2a that I was
    using, it's a little too demanding for my customers.

--- check_bit_field_mapping.h
+++ check_bit_field_mapping.h
@@ -1,32 +1,51 @@
 inline void check_bit_field_mapping()
 {
 	struct {
-		uint32_t flag1: 1 = 1u;
-		uint32_t value1: 4 = 11u;
-		uint32_t flag2: 1 = 1u;
-		uint32_t flag3: 1 = 0;
-		uint32_t value2: 7 = 53u;
+		uint32_t flag1: 1;
+		uint32_t value1: 4;
+		uint32_t flag2: 1;
+		uint32_t flag3: 1;
+		uint32_t value2: 7;
 		uint32_t : 17;
-		uint32_t flag4: 1 = 0;
-	} bit_fields1{};
-	if (*reinterpret_cast<const uint32_t*>(&bit_fields1) != (1u | (11u << 1u) | (1u << 5u) | (53u << 7u)))
+		uint32_t flag4: 1;
+	} bit_fields1{
+		.flag1 = 1u,
+		.value1 = 11u,
+		.flag2 = 1u,
+		.flag3 = 0,
+		.value2 = 53u,
+		.flag4 = 0,
+	};
+	if ((*reinterpret_cast<const uint32_t*>(&bit_fields1) & 0x80003fff) != (1u | (11u << 1u) | (1u << 5u) |
+	                                                                        (53u << 7u)))
 		for (;;)
 			; // bit field mapping problem, halt
 	struct {
-		uint32_t flag1: 1 = 0;
-		uint32_t value1: 4 = 13u;
-		uint32_t flag2: 1 = 1u;
-		uint32_t flag3: 1 = 0;
+		uint32_t flag1: 1;
+		uint32_t value1: 4;
+		uint32_t flag2: 1;
+		uint32_t flag3: 1;
 		uint32_t : 14;
-		uint32_t value2: 3 = 5u;
+		uint32_t value2: 3;
 		uint32_t : 4;
-		uint32_t flag4: 1 = 1u;
-		uint32_t flag5: 1 = 1u;
-		uint32_t flag6: 1 = 0;
-		uint32_t flag7: 1 = 1u;
-	} bit_fields2{};
-	if (*reinterpret_cast<const uint32_t*>(&bit_fields2) != ((13u << 1u) | (1u << 5u) | (5u << 21u) | (1u << 28u) |
-	                                                        (1u << 29u) | (1u << 31u)))
+		uint32_t flag4: 1;
+		uint32_t flag5: 1;
+		uint32_t flag6: 1;
+		uint32_t flag7: 1;
+	} bit_fields2{
+		.flag1 = 0,
+		.value1 = 13u,
+		.flag2 = 1u,
+		.flag3 = 0,
+		.value2 = 5u,
+		.flag4 = 1u,
+		.flag5 = 1u,
+		.flag6 = 0,
+		.flag7 = 1u,
+	};
+	if ((*reinterpret_cast<const uint32_t*>(&bit_fields2) & 0xf0e0007f) != ((13u << 1u) | (1u << 5u) |
+	                                                                        (5u << 21u) | (1u << 28u) |
+	                                                                        (1u << 29u) | (1u << 31u)))
 		for (;;)
 			; // bit field mapping problem, halt
 }
---
+++ test.cpp
@@ -0,0 +1,5 @@
+/*
+ * The only purpose of this file is to have a proper translation unit that includes our generated code, for compilation
+ * test purposes.
+ */
+#include "mcu.h"
